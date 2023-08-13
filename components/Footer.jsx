4ff8b96f52bf82686f3b43cbd0fb9d3734a73ed9
nickburczyk <nick.burczyk@gmail.com>
2023-08-13 13:15:30 +0200

    feat: support dark mode

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { palette } from '../assets/palette';
 
 export const Footer = () => {
+  const colorScheme = useColorScheme()
+  const darkMode = colorScheme === 'dark';
+
+  const containerStyles = [
+    styles.container,
+    { backgroundColor: darkMode ? palette.gray.hex : palette.yellow.hex }
+  ]
+
+  const titleStyles = [
+    styles.title,
+    { color: darkMode ? palette.white.hex : palette.black.hex }
+  ]
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-      🍗 All rights reserved by Wingamabobs &copy;{new Date().getFullYear()}
+    <View style={containerStyles}>
+      <Text style={titleStyles}>
+        🍗 All rights reserved by Wingamabobs &copy;{new Date().getFullYear()}
+        <Text style={{marginLeft: 'auto'}}>{darkMode ? '🌙' : '🌞'}</Text>
       </Text>
     </View>
   );
@@ -20,6 +33,6 @@ const styles = StyleSheet.create({
     color: palette.white.hex, 
     padding: 14, 
     fontSize: 14, 
-    fontWeight: '100'
+    fontWeight: '100',
   }
 })
